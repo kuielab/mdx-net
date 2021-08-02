845e9b4c7c73fb3600140973b15083119eaea567
Minseok Kim <rlaalstjr47@korea.ac.kr>
2021-08-02 17:32:07 +0900

    changed validation process

---
+++ src/datamodules/datasets/Musdb.py
@@ -0,0 +1,100 @@
+import os
+from abc import ABCMeta, ABC
+from pathlib import Path
+
+import soundfile
+from torch.utils.data import Dataset
+import torch
+import numpy as np
+import random
+from tqdm import tqdm
+
+from src.utils.utils import load_wav
+
+
+def check_target_name(target_name, source_names):
+    try:
+        assert target_name is not None
+    except AssertionError:
+        print('[ERROR] please identify target name. ex) +datamodule.target_name="vocals"')
+        exit(-1)
+    try:
+        assert target_name in source_names
+    except AssertionError:
+        print('[ERROR] target name should one of "bass", "drums", "other", "vocals"')
+        exit(-1)
+
+
+def check_sampling_rate(sr, sample_track):
+    try:
+        sampling_rate = soundfile.read(sample_track)[1]
+        assert sampling_rate == sr
+    except AssertionError:
+        sampling_rate = soundfile.read(sample_track)[1]
+        print('[ERROR] sampling rate mismatched')
+        print('\t=> sr in Config file: {}, but sr of data: {}'.format(sr, sampling_rate))
+        exit(-1)
+
+
+class MusdbDataset(Dataset):
+
+    def __init__(self, data_dir, split, aug_params, target_name, sampling_size, external_datasets):
+        super(MusdbDataset, self).__init__()
+
+        self.source_names = ['bass', 'drums', 'other', 'vocals']
+        self.target_name = target_name
+        check_target_name(self.target_name, self.source_names)
+
+        self.sampling_size = sampling_size
+
+        musdb_path = Path(data_dir)
+
+        if not musdb_path.joinpath('metadata').exists():
+            os.mkdir(musdb_path.joinpath('metadata'))
+
+        # create lists of paths for datasets and metadata (track names and duration)
+        dataset_names = [musdb_path.joinpath(split)]
+        metadata_caches = [musdb_path.joinpath('metadata').joinpath(split+'.pkl')]
+        max_pitch, max_tempo = aug_params
+        for p in range(-max_pitch, max_pitch+1):
+            for t in range(-max_tempo, max_tempo+1, 10):
+                if (p, t) == (0, 0):  # no aug
+                    pass
+                else:
+                    aug_split = split + f'_p={p}_t={t}'
+                    dataset_names.append(musdb_path.joinpath(aug_split))
+                    metadata_caches.append(musdb_path.joinpath('metadata').joinpath(aug_split + '.pkl'))
+
+        # collect all track names and their duration
+        self.metadata = []
+        for i, (dataset, metadata_cache) in enumerate(tqdm(zip(dataset_names, metadata_caches))):
+            try:
+                metadata = torch.load(metadata_cache)
+            except FileNotFoundError:
+                print('creating metadata for', dataset)
+                metadata = []
+                for track_name in sorted(os.listdir(dataset)):
+                    track_path = dataset.joinpath(track_name)
+                    track_length = load_wav(track_path.joinpath('vocals.wav')).shape[-1]
+                    metadata.append((track_path, track_length))
+                torch.save(metadata, metadata_cache)
+
+            self.metadata += metadata
+
+            if i == 0:  # get epoch size
+                lengths = [length for path, length in self.metadata]
+                self.num_iter = sum(lengths) // self.sampling_size + 1
+
+    def __getitem__(self, _):
+        sources = []
+        for s_name in self.source_names:
+            track_path, track_length = random.choice(self.metadata)   # random mixing between tracks
+            source = load_wav(track_path.joinpath(s_name + '.wav'),
+                              track_length=track_length, chunk_size=self.sampling_size)
+            sources.append(source)
+        mix = sum(sources)
+        target = sources[self.source_names.index(self.target_name)]
+        return torch.from_numpy(mix), torch.from_numpy(target)
+
+    def __len__(self):
+        return self.num_iter
--- src/models/mdxnet.py
+++ src/models/mdxnet.py
@@ -41,38 +41,24 @@ class AbstractMDXNet(LightningModule):
         pass
 
     def training_step(self, *args, **kwargs) -> STEP_OUTPUT:
-        mixture_wav, target_wav = args[0]
+        mix_wave, target_wave = args[0]
+        mix_spec = self.stft(mix_wave)
 
-        mix_spec = self.stft(mixture_wav)
-        tar_spec = self.stft(target_wav)
-        tar_spec_hat = self(mix_spec)
-        loss = mse_loss(tar_spec_hat, tar_spec)
-        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        target_wave_hat = self.istft(self(mix_spec))
+        loss = mse_loss(target_wave_hat, target_wave)
+        self.log("train/loss", loss, prog_bar=True, sync_dist=True, on_step=True, on_epoch=True)
 
         return {"loss": loss}
 
     def validation_step(self, *args, **kwargs) -> Optional[STEP_OUTPUT]:
-        num_tracks, batch_size, index, mixture_wav_batched, target_wav = args[0]
-        num_tracks, batch_size, index = num_tracks.item(), batch_size.item(), index.item()
+        mix_wave, target_wave = args[0]
+        mix_spec = self.stft(mix_wave)
 
-        target_wav_hats = []
+        target_wave_hat = self.istft(self(mix_spec))
+        loss = sdr(target_wave_hat, target_wave)
+        self.log("val/sdr", loss, logger=True, sync_dist=True, reduce_fx=torch.median, sync_dist_op="median")
 
-        for mixture_wav in mixture_wav_batched[0].split(batch_size):
-            mix_spec = self.stft(mixture_wav)
-            spec_hat = self(mix_spec)
-            target_wav_hat = self.istft(spec_hat)
-            target_wav_hat = target_wav_hat.cpu().detach().numpy()
-            target_wav_hats.append(target_wav_hat)
-
-        target_wav_hat = np.vstack(target_wav_hats)[:, :, self.trim:-self.trim]
-        target_wav_hat = np.concatenate(target_wav_hat, axis=-1)[:, :target_wav.shape[-1]]
-        loss = sdr(target_wav[0].cpu().detach().numpy(), target_wav_hat)/num_tracks
-        self.log("val/sdr", loss, False, True, False, True,
-                 reduce_fx=torch.sum,
-                 sync_dist=True,
-                 sync_dist_op="sum")
-
-        return {'track_id': index, 'track': target_wav_hat}
+        return {'loss': loss}
 
     def stft(self, x):
         x = x.reshape([-1, self.sampling_size])
